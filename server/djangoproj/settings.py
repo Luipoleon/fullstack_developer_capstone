f4a41482f41b69823380dbbccbf990ceccc239bb
Luipoleon <95248473+Luipoleon@users.noreply.github.com>
2024-06-06 18:37:03 -0600

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -5,7 +5,8 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = 'django-insecure-ccow$tz_=9%dxu4(0%^(z%nx32#s@(zt9$ih@)5l54yny)wm-0'
+SECRET_KEY = 'django-insecure-ccow$tz_=9%dxu4\
+              (0%^(z%nx32#s@(zt9$ih@)5l54yny)wm-0'
 
 DEBUG = True
 
@@ -71,16 +72,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.\
+                 UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation\
+                 .MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation\
+                 .CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation\
+                 .NumericPasswordValidator',
     },
 ]
 
